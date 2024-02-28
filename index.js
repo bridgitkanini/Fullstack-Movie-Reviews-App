70c50d44f151caa65b3a05223c47037073168e28
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-02-29 01:15:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import "dotenv/config";
 import app from "./server.js";
 import mongodb from "mongodb";
-// import ReviewsDAO from "./dao/reviewsDAO.js";
+import ReviewsDAO from "./dao/reviewsDAO.js";
+
 
 const MongoClient = mongodb.MongoClient;
 const mongo_username = process.env["MONGO_USERNAME"];
@@ -20,6 +21,7 @@ MongoClient.connect(uri, {
     process.exit(1);
   })
   .then(async (client) => {
+    await ReviewsDAO.injectDB(client);
     app.listen(port, () => {
       console.log(`Listening on port ${port}`);
     });
