6ed71c9e11b7bfdb193940839fe6a3a3538f2c70
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-02-28 18:29:27 +0300

    Update

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+import app from "./server.js";
+import mongodb from "mongodb";
+// import ReviewsDAO from "./dao/reviewsDAO.js";
+
+const MongoClient = mongodb.MongoClient;
+const mongo_username = process.env["MONGO_USERNAME"];
+const mongo_password = process.env["MONGO_PASSWORD"];
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.4zzek7j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`; //Backticks instead of "" to enable ${}, i.e access variables.
+
+const port = 8000;
+
+MongoClient.connect(uri, {
+  maxPoolSize: 50, //Nummber of people connected at the same time.
+  wtimeoutMS: 2500,
+  useNewUrlParser: true, //Huh??
+})
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  })
+  .then(async (client) => {
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    });
+  });
--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("CineVerse here we go!");
+import express from "express";
+import cors from "cors";
+import reviews from "./api/reviews.route.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/v1/reviews", reviews); //v1 not necessary here, but it's good practice to include in case the API has future versions.
+app.use("*", (req, res) => {
+    res.status(404).json({error: "Not found."})
+});
+
+export default app;
